51b9362d85f96a62a5f5a6b48addca8e9e811a74
rani.ko <rani.ko@kakaocorp.com>
2022-10-02 00:53:02 +0900

    Add admin logics

--- settings.py
+++ settings.py
@@ -1,17 +1,19 @@
 CHANGE = 10 ** 6
 MAX_STOCK = 10
-DRINK_POCKET_COUNT = 10
-DRINK_STOCK = {
-    '파워에이드': 9,
-    '게토레이': 10,
-    '하늘보리': 8,
-    '솔의눈': 9,
-    '포카리스웨트': 10,
-    '환타(오렌지)': 9,
-    '코카콜라': 4,
-    '칠성사이다': 5,
-    '펩시콜라': 10,
-}
+DRINK_POCKET_SIZE = 10
+DRINK_KINDS = ['파워에이드', '게토레이', '포카리스웨트', '하늘보리', '솔의눈', '환타', '코카콜라', '칠성사이다', '펩시콜라']
+DRINK_STOCK = [
+    {'name': DRINK_KINDS[0], 'stock': 1},
+    {'name': DRINK_KINDS[1], 'stock': 2},
+    {'name': DRINK_KINDS[2], 'stock': 4},
+    {'name': DRINK_KINDS[3], 'stock': 6},
+    {'name': DRINK_KINDS[4], 'stock': 7},
+    {'name': DRINK_KINDS[5], 'stock': 8},
+    {'name': DRINK_KINDS[6], 'stock': 8},
+    {'name': DRINK_KINDS[7], 'stock': 8},
+    {'name': DRINK_KINDS[8], 'stock': 8},
+    {'name': DRINK_KINDS[8], 'stock': 10},
+]
 SALES = {
     100: 5,
     500: 4,
