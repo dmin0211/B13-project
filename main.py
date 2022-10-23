ead9918047831a767c41ae25d6e72801d8c8cd9a
jyunee <95419701+jynnnnnn@users.noreply.github.com>
2022-10-23 15:37:11 +0900

    main change

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from consumer import process_consumer
 
 import os
 
-admin_manual_mapping = {
+main_manual_mapping = {
     'process_consumer': ['1', '소비자'],
     'process_admin': ['2', '관리자'],
     'process_exit': ['3', '종료'],
@@ -14,15 +14,17 @@ admin_manual_mapping = {
 invalid_input_type = {
     'invalid_manual_command': '존재하지 않는 명령어입니다.',
     'invalid_int_type': '숫자만 입력해주세요.',
-    'invalid_positive_number': '0 초과 양수만 입력해주세요.',
+    'invalid_positive_number': '0이상의 정수만 입력해주세요.',
 }
 
 def transport_positive_number(input_value):
-    if input_value.isnumeric() is not True:
+    try:
+        input_value = int(input_value)
+        if input_value < 0:
+            return 'invalid_positive_number'
+        return input_value
+    except:
         return 'invalid_int_type'
-    elif int(input_value) <= 0:
-        return 'invalid_positive_number'
-    return int(input_value)
 
 # custom input
 def custom_input(prompt, transport_func, **kwargs):
@@ -33,10 +35,14 @@ def custom_input(prompt, transport_func, **kwargs):
         result_value = transport_func(input_value, **kwargs)
     return result_value
 
-# processes
-# 메뉴 보기
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):
+        command = 'cls'
+    os.system(command)
+
 def process_view_menu():
-    print('====Main menu====')
+    print("====Main menu====")
     print("1. 소비자")
     print("2. 관리자")
     print("3. 종료")
@@ -45,7 +51,7 @@ def process_exit():
     exit()
 
 def transport_manual_input(input_value):
-    for process_name, valid_input in admin_manual_mapping.items():
+    for process_name, valid_input in main_manual_mapping.items():
         if input_value in valid_input:
             return process_name
     return 'invalid_manual_command'
@@ -55,26 +61,22 @@ def manual_input(prompt):
     process_name = custom_input(prompt, transport_manual_input)
 
     if process_name == 'invalid_manual_command':
+        clearConsole()
         print(invalid_input_type[process_name])
         process_name = manual_input(prompt)
     elif process_name == 'process_view_menu':
         process_name = manual_input(prompt)
     return process_name
 
-def clearConsole():
-    command = 'clear'
-    if os.name in ('nt', 'dos'):
-        command = 'cls'
-    os.system(command)
-
-
 def __main__():
     while True:
         process_name = manual_input('메인 메뉴를 선택해주세요.')
+        
+        clearConsole()
+        globals()[process_name]()
+        clearConsole()
+        
 
-        if process_name != 'process_exit':
-            globals()[process_name]()
-            clearConsole()
 
 
 __main__()
